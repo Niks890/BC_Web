24f1996bcad86c3ec70ff732c849553b117d9ca8
Niks890 <minhtrivo802@gmail.com>
2023-11-06 12:26:20 +0700

    Add new project

---
+++ assets/js/header-bar.js
@@ -0,0 +1,6 @@
+headerbars = document.querySelector(".header-bars");
+headerbars.onclick = function () {
+    headernav = document.querySelector(".header-navbar");
+    headernav.classList.toggle("active");
+}
+
